43dabb0f2206c8d8386a4505265325a2a058e3a4
Miguel Valdez <miguel.urco@jro.igp.gob.pe>
2012-04-04 21:07:16 +0000

    Actualización del test del programa

--- schainpy/TestSChain.py
+++ schainpy/TestSChain.py
@@ -21,6 +21,7 @@ class TestSChain():
     def __init__(self):
         self.setValues()
         self.createObjects()
+        self.setupObjects()
         self.testSChain()
         pass
     
@@ -29,23 +30,45 @@ class TestSChain():
         self.path = '/home/roj-idl71/Data/RAWDATA/DP_Faraday/'
         self.path = '/Users/danielangelsuarezmunoz/Documents/Projects/testWR'
         self.path = '/home/roj-idl71/Data/RAWDATA/IMAGING'
-#        self.path = '/home/roj-idl71/tmp/data'
-        #self.path = '/remote/puma/2004_11/DVD/'
+        #self.path = '/remote/puma/2011_08/E-F_Valley'
+        #self.path = '/remote/puma/2011_12/EEJ+150km+ONAXIS+ESF+Twilight/Twilight/'
+        self.path = '/home/roj-idl71/tmp/data/'
         
         self.ppath = "/home/roj-idl71/tmp/data"
-        self.startDateTime = datetime.datetime(2011,1,24,18,20,0)
-        self.endDateTime = datetime.datetime(2011,1,30,18,10,0)
+        self.startDateTime = datetime.datetime(2011,1,31,0,20,0)
+        self.endDateTime = datetime.datetime(2011,12,5,18,10,0)
     
     def createObjects(self):        
-        
+
+#        self.Obj = Voltage()
+#        self.readerObj = VoltageReader(self.Obj)
+#        self.plotObj = Osciloscope(self.Obj)
+#        self.writerObj = VoltageWriter(self.Obj)
+      
         self.Obj = Spectra()
         self.readerObj = SpectraReader(self.Obj)
         self.plotObj = Spectrum(self.Obj)
 #        self.writerObj = SpectraWriter(self.Obj)
+
+    def setupObjects(self):
         
-        if not(self.readerObj.setup(self.path, self.startDateTime, self.endDateTime, expLabel='')):
+        if not(self.readerObj.setup(self.path, self.startDateTime, self.endDateTime, expLabel='', online = 0)):
             sys.exit(0)
-            
+        
+        print "Parameters:"
+        
+        print "Num profiles: %s" %(self.readerObj.m_SystemHeader.numProfiles)
+        print "Num samples: %s" %(self.readerObj.m_SystemHeader.numSamples)
+        print "Num channels: %s" %(self.readerObj.m_SystemHeader.numChannels)
+        
+        print "Num profiles per block: %s" %(self.readerObj.m_ProcessingHeader.profilesPerBlock)
+        print "Num heights: %s" %(self.readerObj.m_ProcessingHeader.numHeights)
+        print "Num coh int: %s" %(self.readerObj.m_ProcessingHeader.coherentInt)
+        print "Num incoh int: %s" %(self.readerObj.m_ProcessingHeader.incoherentInt)
+        
+        print "Num code: %d" %(self.readerObj.m_ProcessingHeader.numCode)
+        print "Num baud: %d" %(self.readerObj.m_ProcessingHeader.numBaud)
+        
 #        if not(self.writerObj.setup(self.ppath)):
 #            sys.exit(0)
     
@@ -55,7 +78,7 @@ class TestSChain():
         while(True):
             self.readerObj.getData()
             self.plotObj.plotData(zmin=40, zmax=140, showColorbar=True, showPowerProfile=True)
-            
+            #self.plotObj.plotData(idProfile=1, type="power")
 #            self.writerObj.putData()
             
             if self.readerObj.noMoreFiles:
