4117168188c0a0a884ec5316bc7ec7143adb5028
Alexander Valdez <alexander.valdez@jro.igp.gob.pe>
2012-12-13 17:11:22 +0000

    Last Change Test

--- schainpy/gui/testSignalChainGUI.py
+++ schainpy/gui/testSignalChainGUI.py
@@ -4,23 +4,26 @@
 #from PyQt4 import QtCore, QtGuis
 from PyQt4.QtGui import QApplication
 #from PyQt4.QtCore import pyqtSignature
-#from Controller.workspace import Workspace
-#from Controller.mainwindow import InitWindow
-#from Controller.initwindow import InitWindow
-#from Controller.window import window
+
+from viewcontroller.initwindow import InitWindow
 from viewcontroller.mainwindow import MainWindow
-#from viewcontroller.mainwindow2 import UnitProcess
+from viewcontroller.workspace import Workspace
 
 def main():
     import sys
     app = QApplication(sys.argv)
-    #wnd=InitWindow()
-   # wnd=UnitProcess()
-    wnd=MainWindow()
-    #wnd=Workspace()
-    wnd.show()
+    
+#    Welcome=InitWindow()
+#    Welcome.show() 
+    
+#    WorkPathspace=Workspace()
+#    WorkPathspace.show()
+    #Welcome.ContinueBtn.clicked.connect(WorkPathspace.show)    
+    
+    MainGUI=MainWindow()
+#    WorkPathspace.dirOkbtn.clicked.connect(MainGUI.show)
+    MainGUI.show()
     sys.exit(app.exec_())
     
-
 if __name__ == "__main__":
     main()
