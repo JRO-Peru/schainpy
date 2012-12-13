ae84f2d9427089cb984010ada00088347badbf82
Alexander Valdez <alexander.valdez@jro.igp.gob.pe>
2012-12-13 16:44:12 +0000

    ui_initwindow: update de la interface bienvenida.

--- schainpy/gui/viewer/ui_initwindow.py
+++ schainpy/gui/viewer/ui_initwindow.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'C:\Users\alex\ericworkspace\GUIV1\Pantalla.ui'
+# Form implementation generated from reading ui file 'C:\Users\alex\ericworkspace\GUIV1\WelcomeLast.ui'
 #
-# Created: Tue Aug 28 15:10:06 2012
+# Created: Thu Dec 13 10:07:01 2012
 #      by: PyQt4 UI code generator 4.9.4
 #
 # WARNING! All changes made in this file will be lost!
@@ -28,7 +28,10 @@ class Ui_InitWindow(object):
         self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
         self.label = QtGui.QLabel(self.centralWidget)
         font = QtGui.QFont()
-        font.setPointSize(21)
+        font.setFamily(_fromUtf8("Cambria"))
+        font.setPointSize(22)
+        font.setBold(False)
+        font.setWeight(50)
         self.label.setFont(font)
         self.label.setObjectName(_fromUtf8("label"))
         self.verticalLayout.addWidget(self.label)
@@ -39,7 +42,7 @@ class Ui_InitWindow(object):
         self.verticalLayout.addWidget(self.line)
         self.label_2 = QtGui.QLabel(self.centralWidget)
         self.label_2.setText(_fromUtf8(""))
-        self.label_2.setPixmap(QtGui.QPixmap(_fromUtf8("../../Downloads/IMAGENES/w.jpg")))
+        self.label_2.setPixmap(QtGui.QPixmap(_fromUtf8("figure/w.jpg")))
         self.label_2.setScaledContents(True)
         self.label_2.setObjectName(_fromUtf8("label_2"))
         self.verticalLayout.addWidget(self.label_2)
@@ -50,12 +53,12 @@ class Ui_InitWindow(object):
         self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
         spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
         self.horizontalLayout.addItem(spacerItem)
-        self.pushButton_2 = QtGui.QPushButton(self.centralWidget)
-        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
-        self.horizontalLayout.addWidget(self.pushButton_2)
-        self.pushButton = QtGui.QPushButton(self.centralWidget)
-        self.pushButton.setObjectName(_fromUtf8("pushButton"))
-        self.horizontalLayout.addWidget(self.pushButton)
+        self.ExitBtn = QtGui.QPushButton(self.centralWidget)
+        self.ExitBtn.setObjectName(_fromUtf8("ExitBtn"))
+        self.horizontalLayout.addWidget(self.ExitBtn)
+        self.ContinueBtn = QtGui.QPushButton(self.centralWidget)
+        self.ContinueBtn.setObjectName(_fromUtf8("ContinueBtn"))
+        self.horizontalLayout.addWidget(self.ContinueBtn)
         spacerItem1 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
         self.horizontalLayout.addItem(spacerItem1)
         self.horizontalLayout_2.addLayout(self.horizontalLayout)
@@ -65,33 +68,16 @@ class Ui_InitWindow(object):
         self.statusBar = QtGui.QStatusBar(form)
         self.statusBar.setObjectName(_fromUtf8("statusBar"))
         form.setStatusBar(self.statusBar)
-        self.actionImage = QtGui.QAction(form)
-        icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(_fromUtf8(":/newPrefix/Imagui/ROJ.jpg")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.actionImage.setIcon(icon)
-        self.actionImage.setObjectName(_fromUtf8("actionImage"))
-        self.actionOpen = QtGui.QAction(form)
-        self.actionOpen.setObjectName(_fromUtf8("actionOpen"))
-        self.actionClose = QtGui.QAction(form)
-        self.actionClose.setObjectName(_fromUtf8("actionClose"))
-        self.actionOpen_2 = QtGui.QAction(form)
-        self.actionOpen_2.setObjectName(_fromUtf8("actionOpen_2"))
 
         self.retranslateUi(form)
-        QtCore.QObject.connect(self.pushButton_2, QtCore.SIGNAL(_fromUtf8("clicked()")), form.close)
-        QtCore.QObject.connect(self.pushButton, QtCore.SIGNAL(_fromUtf8("clicked()")), form.show)
         QtCore.QMetaObject.connectSlotsByName(form)
 
     def retranslateUi(self, form):
         form.setWindowTitle(QtGui.QApplication.translate("form", "MainWindow", None, QtGui.QApplication.UnicodeUTF8))
-        self.label.setText(QtGui.QApplication.translate("form", "Interfaz Grafica -REV. 1.0", None, QtGui.QApplication.UnicodeUTF8))
-        self.pushButton_2.setText(QtGui.QApplication.translate("form", "Exit", None, QtGui.QApplication.UnicodeUTF8))
-        self.pushButton.setText(QtGui.QApplication.translate("form", "Continue", None, QtGui.QApplication.UnicodeUTF8))
-        self.actionImage.setText(QtGui.QApplication.translate("form", "image", None, QtGui.QApplication.UnicodeUTF8))
-        self.actionImage.setToolTip(QtGui.QApplication.translate("form", "show Image", None, QtGui.QApplication.UnicodeUTF8))
-        self.actionOpen.setText(QtGui.QApplication.translate("form", "Open", None, QtGui.QApplication.UnicodeUTF8))
-        self.actionClose.setText(QtGui.QApplication.translate("form", "Close", None, QtGui.QApplication.UnicodeUTF8))
-        self.actionOpen_2.setText(QtGui.QApplication.translate("form", "Open", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("form", "Signal Chain - Ver. 1.0", None, QtGui.QApplication.UnicodeUTF8))
+        self.ExitBtn.setText(QtGui.QApplication.translate("form", "Exit", None, QtGui.QApplication.UnicodeUTF8))
+        self.ContinueBtn.setText(QtGui.QApplication.translate("form", "Continue", None, QtGui.QApplication.UnicodeUTF8))
+
 
 if __name__ == "__main__":
     import sys
