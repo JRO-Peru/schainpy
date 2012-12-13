1da5a2d48804963aeb5d153b5a368bdf85f0127d
Alexander Valdez <alexander.valdez@jro.igp.gob.pe>
2012-12-13 17:10:15 +0000

    update workspace

--- schainpy/gui/viewcontroller/workspace.py
+++ schainpy/gui/viewcontroller/workspace.py
@@ -4,8 +4,8 @@ from PyQt4.QtGui import QMainWindow
 from PyQt4.QtCore import pyqtSignature
 from PyQt4.QtCore import pyqtSignal
 from PyQt4 import QtGui
-from mainwindow import MainWindow
-from GUI.ui_workspace import Ui_Workspace
+from viewcontroller.mainwindow import MainWindow
+from viewer.ui_workspace import Ui_Workspace
 
 class Workspace(QMainWindow, Ui_Workspace):
     """
@@ -17,33 +17,42 @@ class Workspace(QMainWindow, Ui_Workspace):
         Constructor
         """
         QMainWindow.__init__(self, parent)
+        self.dirList=[]
         self.setupUi(self)
         #*#######   DIRECTORIO DE TRABAJO  #########*#
         self.dirCmbBox.setItemText(0, QtGui.QApplication.translate("MainWindow", "C:\WorkSpaceGui", None, QtGui.QApplication.UnicodeUTF8))
         self.dir=str("C:\WorkSpaceGui")
         self.dirCmbBox.addItem(self.dir)
+        self.dirList.append(self.dir)
                   
     @pyqtSignature("")
     def on_dirBrowsebtn_clicked(self):
         """
         Slot documentation goes here.
         """
+        
         self.dirBrowse = str(QtGui.QFileDialog.getExistingDirectory(self, 'Open Directory', './',  QtGui.QFileDialog.ShowDirsOnly))
-        self.dirCmbBox.addItem(self.dirBrowse)
+        self.dirCmbBox.setItemText(0, QtGui.QApplication.translate("MainWindow", self.dirBrowse, None, QtGui.QApplication.UnicodeUTF8))
+        self.dirList.append(self.dirBrowse)
+        self.dirCmbBox.clear()
+        for i in self.dirList:
+            self.dirCmbBox.addItem(i)
+        
+
         
     @pyqtSignature("")
     def on_dirButton_clicked(self):
         """
         Slot documentation goes here.
         """
+        pass
 
     @pyqtSignature("")
     def on_dirOkbtn_clicked(self):
         """
         VISTA DE INTERFAZ GRÃFICA
         """
-        self.showmemainwindow()
-    
+        self.close()
                
     @pyqtSignature("")
     def on_dirCancelbtn_clicked(self):
@@ -51,11 +60,6 @@ class Workspace(QMainWindow, Ui_Workspace):
         Cerrar
         """
         self.close()
-        
-    def showmemainwindow(self):
-        self.Dialog= MainWindow(self)
-        self.Dialog.show()
-        self.hide()
 
     def closeEvent(self, event):
         self.closed.emit()
