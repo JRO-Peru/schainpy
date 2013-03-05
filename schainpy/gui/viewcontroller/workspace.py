fb193e013596f279b5239230b0581efbdb5de2c3
Alexander Valdez <alexander.valdez@jro.igp.gob.pe>
2013-03-05 21:55:04 +0000

    Adicion de Icono Mejor identificacion de comandos

--- schainpy/gui/viewcontroller/workspace.py
+++ schainpy/gui/viewcontroller/workspace.py
@@ -4,7 +4,6 @@ from PyQt4.QtGui import QMainWindow
 from PyQt4.QtCore import pyqtSignature
 from PyQt4.QtCore import pyqtSignal
 from PyQt4 import QtGui
-from viewcontroller.mainwindow import MainWindow
 from viewer.ui_workspace import Ui_Workspace
 
 class Workspace(QMainWindow, Ui_Workspace):
@@ -19,43 +18,37 @@ class Workspace(QMainWindow, Ui_Workspace):
         QMainWindow.__init__(self, parent)
         self.dirList=[]
         self.setupUi(self)
+        self.setWindowTitle("ROJ-Signal Chain")
+        self.setWindowIcon(QtGui.QIcon("figure/adn.jpg"))
         #*#######   DIRECTORIO DE TRABAJO  #########*#
-        self.dirCmbBox.setItemText(0, QtGui.QApplication.translate("MainWindow", "C:\WorkSpaceGui", None, QtGui.QApplication.UnicodeUTF8))
+        #self.dirCmbBox.setItemText(0, QtGui.QApplication.translate("MainWindow", "C:\WorkSpaceGui", None, QtGui.QApplication.UnicodeUTF8))
+        
         self.dir=str("C:\WorkSpaceGui")
-        self.dirCmbBox.addItem(self.dir)
-        self.dirList.append(self.dir)
+        self.dirComBox.addItem(self.dir)
+        self.i=0
+        
                   
     @pyqtSignature("")
-    def on_dirBrowsebtn_clicked(self):
+    def on_dirToolPath_clicked(self):
         """
         Slot documentation goes here.
         """
-        
+        self.i +=1
         self.dirBrowse = str(QtGui.QFileDialog.getExistingDirectory(self, 'Open Directory', './',  QtGui.QFileDialog.ShowDirsOnly))
-        self.dirCmbBox.setItemText(0, QtGui.QApplication.translate("MainWindow", self.dirBrowse, None, QtGui.QApplication.UnicodeUTF8))
-        self.dirList.append(self.dirBrowse)
-        self.dirCmbBox.clear()
-        for i in self.dirList:
-            self.dirCmbBox.addItem(i)
-        
+        self.dirComBox.addItem(self.dirBrowse)
+        self.dirComBox.setCurrentIndex(self.i)
 
-        
-    @pyqtSignature("")
-    def on_dirButton_clicked(self):
-        """
-        Slot documentation goes here.
-        """
-        pass
+
 
     @pyqtSignature("")
-    def on_dirOkbtn_clicked(self):
+    def on_dirOkBtn_clicked(self):
         """
         VISTA DE INTERFAZ GRÃFICA
         """
         self.close()
                
     @pyqtSignature("")
-    def on_dirCancelbtn_clicked(self):
+    def on_dirCancelBtn_clicked(self):
         """
         Cerrar
         """
