d161276043374c5d8c0ad08d8903489e70e91241
Alexander Valdez <alexander.valdez@jro.igp.gob.pe>
2013-03-05 21:56:38 +0000

    Ultimo test de la Interfaz gráfica. Falta Corregir el workspace

--- schainpy/gui/testSignalChainGUI.py
+++ schainpy/gui/testSignalChainGUI.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-'
 
 #from PyQt4 import QtCore, QtGuis
 from PyQt4.QtGui import QApplication
 #from PyQt4.QtCore import pyqtSignature
 
-#from viewcontroller.initwindow import InitWindow
+from viewcontroller.initwindow import InitWindow
 from viewcontroller.basicwindow import BasicWindow
 #from viewcontroller.mainwindow import BodyMainWindow
 #from viewer.ui_mainwindow import Ui_MainWindow
-#from viewcontroller.workspace import Workspace
+from viewcontroller.workspace import Workspace
 
 def main():
     import sys
     app = QApplication(sys.argv)
     
-#    Welcome=InitWindow()
-#    Welcome.show() 
+    Welcome=InitWindow()
+    Welcome.show() 
 #    
-#    WorkPathspace=Workspace()
+    WorkPathspace=Workspace()
     #WorkPathspace.show()
-#    Welcome.ContinueBtn.clicked.connect(WorkPathspace.show)    
+    Welcome.ContinueBtn.clicked.connect(WorkPathspace.show)    
 #    
 #    if self.isBasic():
 #        MainGUI = BasicWindow()
@@ -28,8 +28,8 @@ def main():
 #        MainGUI = AdvancedWindow()
         
     MainGUI=BasicWindow()
-#    WorkPathspace.dirOkbtn.clicked.connect(MainGUI.show)
-    MainGUI.show()
+    WorkPathspace.closed.connect(MainGUI.show)
+   # MainGUI.show()
     sys.exit(app.exec_())
     
 if __name__ == "__main__":
