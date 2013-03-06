c4d9885579a36d51aa49fa6cf8338181c7cb36d4
Alexander Valdez <alexander.valdez@jro.igp.gob.pe>
2013-03-06 20:40:29 +0000

    ui_initwindow: Ahora hereda las provpiedas de una Clase QDialog

--- schainpy/gui/viewer/ui_initwindow.py
+++ schainpy/gui/viewer/ui_initwindow.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'C:\Users\alex\ericworkspace\GUIV1\WelcomeLast.ui'
+# Form implementation generated from reading ui file '/home/roj-idl71/SignalChain/initwindowv2.ui'
 #
-# Created: Thu Dec 13 10:07:01 2012
-#      by: PyQt4 UI code generator 4.9.4
+# Created: Wed Mar  6 15:32:39 2013
+#      by: PyQt4 UI code generator 4.8.6
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -15,76 +15,74 @@ except AttributeError:
     _fromUtf8 = lambda s: s
 
 class Ui_InitWindow(object):
-    def setupUi(self, form):
-        form.setObjectName(_fromUtf8("form"))
-        form.resize(622, 516)
-        self.centralWidget = QtGui.QWidget(form)
-        self.centralWidget.setObjectName(_fromUtf8("centralWidget"))
-        self.gridLayout = QtGui.QGridLayout(self.centralWidget)
+    def setupUi(self, Dialog):
+        Dialog.setObjectName(_fromUtf8("Dialog"))
+        Dialog.resize(652, 496)
+        Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "Dialog", None, QtGui.QApplication.UnicodeUTF8))
+        self.gridLayout = QtGui.QGridLayout(Dialog)
         self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
-        self.verticalLayout_2 = QtGui.QVBoxLayout()
-        self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
-        self.verticalLayout = QtGui.QVBoxLayout()
-        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-        self.label = QtGui.QLabel(self.centralWidget)
+        self.verticalLayout_3 = QtGui.QVBoxLayout()
+        self.verticalLayout_3.setObjectName(_fromUtf8("verticalLayout_3"))
+        self.verticalLayout_4 = QtGui.QVBoxLayout()
+        self.verticalLayout_4.setObjectName(_fromUtf8("verticalLayout_4"))
+        self.label_3 = QtGui.QLabel(Dialog)
         font = QtGui.QFont()
         font.setFamily(_fromUtf8("Cambria"))
         font.setPointSize(22)
         font.setBold(False)
         font.setWeight(50)
-        self.label.setFont(font)
-        self.label.setObjectName(_fromUtf8("label"))
-        self.verticalLayout.addWidget(self.label)
-        self.line = QtGui.QFrame(self.centralWidget)
-        self.line.setFrameShape(QtGui.QFrame.HLine)
-        self.line.setFrameShadow(QtGui.QFrame.Sunken)
-        self.line.setObjectName(_fromUtf8("line"))
-        self.verticalLayout.addWidget(self.line)
-        self.label_2 = QtGui.QLabel(self.centralWidget)
-        self.label_2.setText(_fromUtf8(""))
-        self.label_2.setPixmap(QtGui.QPixmap(_fromUtf8("figure/w.jpg")))
-        self.label_2.setScaledContents(True)
-        self.label_2.setObjectName(_fromUtf8("label_2"))
-        self.verticalLayout.addWidget(self.label_2)
-        self.verticalLayout_2.addLayout(self.verticalLayout)
-        self.horizontalLayout_2 = QtGui.QHBoxLayout()
-        self.horizontalLayout_2.setObjectName(_fromUtf8("horizontalLayout_2"))
-        self.horizontalLayout = QtGui.QHBoxLayout()
-        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.label_3.setFont(font)
+        self.label_3.setText(QtGui.QApplication.translate("Dialog", "Signal Chain - Ver. 1.0", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.verticalLayout_4.addWidget(self.label_3)
+        self.line_2 = QtGui.QFrame(Dialog)
+        self.line_2.setFrameShape(QtGui.QFrame.HLine)
+        self.line_2.setFrameShadow(QtGui.QFrame.Sunken)
+        self.line_2.setObjectName(_fromUtf8("line_2"))
+        self.verticalLayout_4.addWidget(self.line_2)
+        self.label_4 = QtGui.QLabel(Dialog)
+        self.label_4.setText(_fromUtf8(""))
+        self.label_4.setPixmap(QtGui.QPixmap(_fromUtf8("figure/w.jpg")))
+        self.label_4.setScaledContents(True)
+        self.label_4.setObjectName(_fromUtf8("label_4"))
+        self.verticalLayout_4.addWidget(self.label_4)
+        self.verticalLayout_3.addLayout(self.verticalLayout_4)
+        self.horizontalLayout_3 = QtGui.QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(_fromUtf8("horizontalLayout_3"))
+        self.horizontalLayout_4 = QtGui.QHBoxLayout()
+        self.horizontalLayout_4.setObjectName(_fromUtf8("horizontalLayout_4"))
         spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem)
-        self.ExitBtn = QtGui.QPushButton(self.centralWidget)
+        self.horizontalLayout_4.addItem(spacerItem)
+        self.ExitBtn = QtGui.QPushButton(Dialog)
+        self.ExitBtn.setText(QtGui.QApplication.translate("Dialog", "Exit", None, QtGui.QApplication.UnicodeUTF8))
         self.ExitBtn.setObjectName(_fromUtf8("ExitBtn"))
-        self.horizontalLayout.addWidget(self.ExitBtn)
-        self.ContinueBtn = QtGui.QPushButton(self.centralWidget)
+        self.horizontalLayout_4.addWidget(self.ExitBtn)
+        self.ContinueBtn = QtGui.QPushButton(Dialog)
+        self.ContinueBtn.setText(QtGui.QApplication.translate("Dialog", "Continue", None, QtGui.QApplication.UnicodeUTF8))
         self.ContinueBtn.setObjectName(_fromUtf8("ContinueBtn"))
-        self.horizontalLayout.addWidget(self.ContinueBtn)
+        self.horizontalLayout_4.addWidget(self.ContinueBtn)
         spacerItem1 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem1)
-        self.horizontalLayout_2.addLayout(self.horizontalLayout)
-        self.verticalLayout_2.addLayout(self.horizontalLayout_2)
-        self.gridLayout.addLayout(self.verticalLayout_2, 0, 0, 1, 1)
-        form.setCentralWidget(self.centralWidget)
-        self.statusBar = QtGui.QStatusBar(form)
-        self.statusBar.setObjectName(_fromUtf8("statusBar"))
-        form.setStatusBar(self.statusBar)
+        self.horizontalLayout_4.addItem(spacerItem1)
+        self.horizontalLayout_3.addLayout(self.horizontalLayout_4)
+        self.verticalLayout_3.addLayout(self.horizontalLayout_3)
+        self.gridLayout.addLayout(self.verticalLayout_3, 0, 0, 1, 1)
 
-        self.retranslateUi(form)
-        QtCore.QMetaObject.connectSlotsByName(form)
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
 
-    def retranslateUi(self, form):
-        form.setWindowTitle(QtGui.QApplication.translate("form", "MainWindow", None, QtGui.QApplication.UnicodeUTF8))
-        self.label.setText(QtGui.QApplication.translate("form", "Signal Chain - Ver. 1.0", None, QtGui.QApplication.UnicodeUTF8))
-        self.ExitBtn.setText(QtGui.QApplication.translate("form", "Exit", None, QtGui.QApplication.UnicodeUTF8))
-        self.ContinueBtn.setText(QtGui.QApplication.translate("form", "Continue", None, QtGui.QApplication.UnicodeUTF8))
+    def retranslateUi(self, Dialog):
+        pass
 
 
 if __name__ == "__main__":
     import sys
     app = QtGui.QApplication(sys.argv)
-    form = QtGui.QMainWindow()
+    Dialog = QtGui.QDialog()
     ui = Ui_InitWindow()
-    ui.setupUi(form)
-    form.show()
+    ui.setupUi(Dialog)
+    Dialog.show()
     sys.exit(app.exec_())
+
+    
+    
 
