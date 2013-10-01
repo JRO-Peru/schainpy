a0e651e86fe89b1e1f37a8801354d2a421f3f9df
Daniel Valdez <daniel.suarez@jro.igp.gob.pe>
2013-10-01 19:45:18 +0000

    Scripts para procesamiento y ploteo del Experimento Imaging+ISR Setiembre 2013

--- schainpy/test/Imaging_Plots.py
+++ schainpy/test/Imaging_Plots.py
@@ -12,15 +12,15 @@ controllerObj = Project()
 
 controllerObj.setup(id = '191', name='test01', description=desc)
 
-path = '/media/datos/IMAGING/IMAGING'
+path = '/home/dsuarez/EW_Faraday_imaging'
+path = '/media/datos/IMAGING/IMAGING/setiembre2013'
 
 readUnitConfObj = controllerObj.addReadUnit(datatype='Spectra',
                                             path=path,
-                                            startDate='2013/04/09',
-                                            endDate='2013/04/09',
-                                            startTime='17:00:00',
+                                            startDate='2013/09/27',
+                                            endDate='2013/09/27',
+                                            startTime='19:00:00',
                                             endTime='23:59:59',
-                                            set=0,
                                             delay=20,
                                             online=1,
                                             walk=1)
@@ -36,43 +36,59 @@ procUnitConfObj1 = controllerObj.addProcUnit(datatype='Spectra', inputId=readUni
 # opObj11.addParameter(name='n', value='2', format='float')
 
 opObj11 = procUnitConfObj1.addOperation(name='SpectraPlot', optype='other')
-opObj11.addParameter(name='id', value='100', format='int')
+opObj11.addParameter(name='id', value='2000', format='int')
 opObj11.addParameter(name='wintitle', value='Imaging', format='str')
-opObj11.addParameter(name='zmin', value='10', format='int')
-opObj11.addParameter(name='zmax', value='50', format='int')
+opObj11.addParameter(name='zmin', value='35', format='int')
+opObj11.addParameter(name='zmax', value='45', format='int')
 # opObj11.addParameter(name='ymin', value='0', format='int')
 # opObj11.addParameter(name='ymax', value='300', format='int')
+opObj11.addParameter(name='save', value='1', format='int')
+opObj11.addParameter(name='figpath', value='/media/datos/IMAGING/IMAGING/graphs/imaging_201309', format='str')
+opObj11.addParameter(name='wr_period', value='5', format='int')
+opObj11.addParameter(name='ftp', value='1', format='int')
+opObj11.addParameter(name='server', value='jro-app.igp.gob.pe', format='str')
+opObj11.addParameter(name='folder', value='/home/wmaster/graficos', format='str')
+opObj11.addParameter(name='username', value='wmaster', format='str')
+opObj11.addParameter(name='password', value='mst2010vhf', format='str')
+opObj11.addParameter(name='ftp_wei', value='0', format='int')
+opObj11.addParameter(name='exp_code', value='13', format='int')
+opObj11.addParameter(name='sub_exp_code', value='0', format='int')
+opObj11.addParameter(name='plot_pos', value='0', format='int')
+
+
 
 
 
 # opObj11 = procUnitConfObj1.addOperation(name='RTIPlot', optype='other')
 # opObj11.addParameter(name='id', value='101', format='int')
 # opObj11.addParameter(name='wintitle', value='Imaging', format='str')
-# opObj11.addParameter(name='xmin', value='0', format='float')
+# opObj11.addParameter(name='xmin', value='19', format='float')
 # opObj11.addParameter(name='xmax', value='24', format='float')
 # opObj11.addParameter(name='save', value='1', format='int')
-# opObj11.addParameter(name='figfile', value='rti-imaging.png', format='str')
-# opObj11.addParameter(name='figpath', value='/media/datos/IMAGING/IMAGING/graphs', format='str')
+# # opObj11.addParameter(name='figfile', value='rti-imaging.png', format='str')
+# opObj11.addParameter(name='figpath', value='/home/dsuarez/Pictures/imaging_last_night', format='str')
 # opObj11.addParameter(name='ftp', value='1', format='int')
 # opObj11.addParameter(name='ftpratio', value='3', format='int')
 # 
 # 
 # 
-# opObj11 = procUnitConfObj1.addOperation(name='CoherenceMap', optype='other')
-# opObj11.addParameter(name='id', value='102', format='int')
-# opObj11.addParameter(name='wintitle', value='Imaging', format='str')
-# opObj11.addParameter(name='xmin', value='0', format='float')
-# opObj11.addParameter(name='xmax', value='24', format='float')
-# #opObj11.addParameter(name='zmin', value='30', format='int')
-# #opObj11.addParameter(name='zmax', value='50', format='int')
-# #opObj11.addParameter(name='xmin', value='18.5', format='float')
-# #opObj11.addParameter(name='xmax', value='22', format='float')
-# opObj11.addParameter(name='save', value='1', format='int')
-# opObj11.addParameter(name='figfile', value='coherence-imaging.png', format='str')
-# opObj11.addParameter(name='figpath', value='/media/datos/IMAGING/IMAGING/graphs', format='str')
-# opObj11.addParameter(name='ftp', value='1', format='int')
-# opObj11.addParameter(name='ftpratio', value='3', format='int')
-
+opObj11 = procUnitConfObj1.addOperation(name='CoherenceMap', optype='other')
+opObj11.addParameter(name='id', value='2001', format='int')
+opObj11.addParameter(name='wintitle', value='Imaging', format='str')
+opObj11.addParameter(name='xmin', value='17', format='float')
+opObj11.addParameter(name='xmax', value='24', format='float')
+opObj11.addParameter(name='save', value='1', format='int')
+opObj11.addParameter(name='figpath', value='/media/datos/IMAGING/IMAGING/graphs/imaging_201309', format='str')
+opObj11.addParameter(name='wr_period', value='5', format='int')
+opObj11.addParameter(name='ftp', value='1', format='int')
+opObj11.addParameter(name='server', value='jro-app.igp.gob.pe', format='str')
+opObj11.addParameter(name='folder', value='/home/wmaster/graficos', format='str')
+opObj11.addParameter(name='username', value='wmaster', format='str')
+opObj11.addParameter(name='password', value='mst2010vhf', format='str')
+opObj11.addParameter(name='ftp_wei', value='0', format='int')
+opObj11.addParameter(name='exp_code', value='13', format='int')
+opObj11.addParameter(name='sub_exp_code', value='0', format='int')
+opObj11.addParameter(name='plot_pos', value='0', format='int')
 
 
 
--- schainpy/test/Imaging_Proc.py
+++ schainpy/test/Imaging_Proc.py
@@ -13,14 +13,14 @@ controllerObj = Project()
 controllerObj.setup(id = '191', name='test01', description=desc)
 
 path = '/remote'
+path = '/home/dsuarez/.gvfs/data on 10.10.20.13/EW_Faraday_imaging/d2013270'
 path = '/home/dsuarez/.gvfs/data on 10.10.20.13/EW_Faraday_imaging'
-# path = '/home/dsuarez/imaging_data'
 
 readUnitConfObj = controllerObj.addReadUnit(datatype='Voltage',
                                             path=path,
-                                            startDate='2013/04/09',
-                                            endDate='2013/04/09',
-                                            startTime='17:00:00',
+                                            startDate='2013/09/27',
+                                            endDate='2013/09/27',
+                                            startTime='16:00:00',
                                             endTime='23:59:59',
                                             delay=20,
                                             online=1,
@@ -32,13 +32,16 @@ opObj11 = readUnitConfObj.addOperation(name='printNumberOfBlock')
 procUnitConfObj0 = controllerObj.addProcUnit(datatype='Voltage', inputId=readUnitConfObj.getId())
 #
 opObj11 = procUnitConfObj0.addOperation(name='ProfileSelector', optype='other')
-opObj11.addParameter(name='profileRangeList', value='133,172', format='intlist')
+opObj11.addParameter(name='profileRangeList', value='0,39', format='intlist')
+# opObj11.addParameter(name='profileRangeList', value='40,167', format='intlist')
 
-#opObj11 = procUnitConfObj0.addOperation(name='filterByHeights')
-#opObj11.addParameter(name='window', value='4', format='int')
+# opObj11 = procUnitConfObj0.addOperation(name='filterByHeights')
+# opObj11.addParameter(name='window', value='4', format='int')
 
 opObj11 = procUnitConfObj0.addOperation(name='Decoder', optype='other')
-
+# opObj11.addParameter(name='code', value='1,0,0,1,0,0,-1,0,0,-1,0,0,-1,0,0,1,0,0', format='floatlist')
+# opObj11.addParameter(name='nCode', value='2', format='int')
+# opObj11.addParameter(name='nBaud', value='9', format='int')
 
 #opObj11 = procUnitConfObj0.addOperation(name='selectHeights')
 #opObj11.addParameter(name='maxHei', value='300', format='float')
@@ -48,8 +51,9 @@ opObj11 = procUnitConfObj0.addOperation(name='Decoder', optype='other')
 #opObj11.addParameter(name='maxHei', value='600', format='float')
 
 procUnitConfObj1 = controllerObj.addProcUnit(datatype='Spectra', inputId=procUnitConfObj0.getId())
-procUnitConfObj1.addParameter(name='nFFTPoints', value='8', format='int')
-procUnitConfObj1.addParameter(name='nProfiles', value='8', format='int')
+procUnitConfObj1.addParameter(name='nProfiles', value='40', format='int')
+procUnitConfObj1.addParameter(name='nFFTPoints', value='40', format='int')
+
 procUnitConfObj1.addParameter(name='pairsList', value='(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),(0,7), \
 							(1,2),(1,3),(1,4),(1,5),(1,6),(1,7), \
 							(2,3),(2,4),(2,5),(2,6),(2,7), \
@@ -60,11 +64,11 @@ procUnitConfObj1.addParameter(name='pairsList', value='(0,1),(0,2),(0,3),(0,4),(
 							format='pairslist')
 
 opObj11 = procUnitConfObj1.addOperation(name='IncohInt', optype='other')
-opObj11.addParameter(name='n', value='50', format='float')
+opObj11.addParameter(name='timeInterval', value='5', format='float')
 
 
 opObj11 = procUnitConfObj1.addOperation(name='SpectraWriter', optype='other')
-opObj11.addParameter(name='path', value='/media/datos/IMAGING/IMAGING')
+opObj11.addParameter(name='path', value='/media/datos/IMAGING/IMAGING/setiembre2013')
 opObj11.addParameter(name='blocksPerFile', value='10', format='int')
 
 
@@ -76,5 +80,3 @@ controllerObj.readXml(filename)
 controllerObj.createObjects()
 controllerObj.connectObjects()
 controllerObj.run()
-  
-  
