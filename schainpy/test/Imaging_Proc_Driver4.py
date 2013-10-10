50e906ea6ae688666fe85cd64af3308e8c5133df
Daniel Valdez <daniel.suarez@jro.igp.gob.pe>
2013-10-10 18:13:11 +0000

    Script mejorado, toma entradas por linea de comando

--- schainpy/test/Imaging_Proc_Driver4.py
+++ schainpy/test/Imaging_Proc_Driver4.py
@@ -5,6 +5,76 @@ sys.path.append(path)
 
 from controller import *
 
+def verifyCmdArguments():   
+    import getopt
+    arglist = ''
+    path = None
+    startDate = None    
+    startTime = None
+    endDate = None
+    endTime = None
+    xmin = None
+    xmax = None
+    zmin = None
+    zmax = None
+    gpath = None
+    wpath = None
+    save_figure = None
+    save_pdata = None
+
+    longarglist = ['path=',
+                   'startDate=',
+                   'startTime=',
+                   'endDate=',
+                   'endTime=',
+                   'xmin=',
+                   'xmax=',
+                   'zmin=',                
+                   'zmax=',
+                   'gpath=',
+                   'wpath=',
+                   'save_figure=',
+                   'save_pdata='
+                   ]
+    
+    optlist, args = getopt.getopt(sys.argv[1:], arglist, longarglist)
+            
+    for opt in optlist:
+        if opt[0] == '--path':
+            path = opt[1]
+        elif opt[0] == '--startDate':
+            startDate = opt[1]
+        elif opt[0] == '--startTime':
+            startTime = opt[1]
+        elif opt[0] == '--endDate':
+            endDate = opt[1]
+        elif opt[0] == '--endTime':
+            endTime = opt[1]
+        elif opt[0] == '--xmin':
+            xmin = opt[1]
+        elif opt[0] == '--xmax':
+            xmax = opt[1]
+        elif opt[0] == '--zmin':
+            zmin = opt[1]
+        elif opt[0] == '--zmax':
+            zmax = opt[1]
+        elif opt[0] == '--gpath':
+            gpath = opt[1]
+        elif opt[0] == '--wpath':
+            wpath = opt[1]
+        elif opt[0] == '--save_figure':
+            save_figure = bool(int(opt[1]))
+        elif opt[0] == '--save_pdata':
+            save_pdata = bool(int(opt[1]))
+                                
+        else:
+            print 'Illegal option %s\n%s%s' % (opt[0], usage, expId.keys())
+            sys.exit(-1)
+    
+    #print path,startDate,startTime,endDate,endTime,xmin,xmax,zmin,zmax
+    return path,startDate,startTime,endDate,endTime,xmin,xmax,zmin,zmax,gpath,save_figure,wpath,save_pdata
+
+
 desc = "EWDrifts+Imaging+Faraday Experiment"
 filename = "imaging_proc.xml"
 
@@ -17,12 +87,15 @@ path = '/home/dsuarez/.gvfs/data on 10.10.20.13/EW_Faraday_imaging/d2013270'
 path = '/home/dsuarez/.gvfs/data on 10.10.20.13/EW_Faraday_imaging/d2013267'
 path = '/home/dsuarez/.gvfs/data on 10.10.20.13/Imaging_Driver4'
 
+path,startDate,startTime,endDate,endTime,xmin,xmax,zmin,zmax,gpath,save_figure,wpath,save_pdata = verifyCmdArguments()
+
+
 readUnitConfObj = controllerObj.addReadUnit(datatype='Voltage',
                                             path=path,
-                                            startDate='2013/09/30',
-                                            endDate='2013/09/30',
-                                            startTime='19:00:00',
-                                            endTime='23:59:59',
+                                            startDate=startDate,
+                                            endDate=endDate,
+                                            startTime=startTime,
+                                            endTime=endTime,
                                             delay=20,
                                             online=0,
                                             walk=1)
@@ -31,29 +104,14 @@ opObj11 = readUnitConfObj.addOperation(name='printNumberOfBlock')
 
 ######################## IMAGING #############################################
 procUnitConfObj0 = controllerObj.addProcUnit(datatype='Voltage', inputId=readUnitConfObj.getId())
-#
-# opObj11 = procUnitConfObj0.addOperation(name='ProfileSelector', optype='other')
-# opObj11.addParameter(name='profileRangeList', value='0,39', format='intlist')
-# opObj11.addParameter(name='profileRangeList', value='40,167', format='intlist')
 
-# opObj11 = procUnitConfObj0.addOperation(name='filterByHeights')
-# opObj11.addParameter(name='window', value='4', format='int')
 
 opObj11 = procUnitConfObj0.addOperation(name='Decoder', optype='other')
-# opObj11.addParameter(name='code', value='1,0,0,1,0,0,-1,0,0,-1,0,0,-1,0,0,1,0,0', format='floatlist')
-# opObj11.addParameter(name='nCode', value='2', format='int')
-# opObj11.addParameter(name='nBaud', value='9', format='int')
 
-#opObj11 = procUnitConfObj0.addOperation(name='selectHeights')
-#opObj11.addParameter(name='maxHei', value='300', format='float')
-
-#opObj11 = procUnitConfObj0.addOperation(name='selectHeights')
-#opObj11.addParameter(name='minHei', value='300', format='float')
-#opObj11.addParameter(name='maxHei', value='600', format='float')
 
 procUnitConfObj1 = controllerObj.addProcUnit(datatype='Spectra', inputId=procUnitConfObj0.getId())
-procUnitConfObj1.addParameter(name='nProfiles', value='40', format='int')
-procUnitConfObj1.addParameter(name='nFFTPoints', value='40', format='int')
+procUnitConfObj1.addParameter(name='nProfiles', value='16', format='int')
+procUnitConfObj1.addParameter(name='nFFTPoints', value='16', format='int')
  
 procUnitConfObj1.addParameter(name='pairsList', value='(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),(0,7), \
                             (1,2),(1,3),(1,4),(1,5),(1,6),(1,7), \
@@ -68,21 +126,34 @@ opObj11 = procUnitConfObj1.addOperation(name='IncohInt', optype='other')
 opObj11.addParameter(name='timeInterval', value='5', format='float')
 
 
+# opObj11 = procUnitConfObj1.addOperation(name='SpectraPlot', optype='other')
+# opObj11.addParameter(name='id', value='2000', format='int')
+# opObj11.addParameter(name='wintitle', value='Imaging', format='str')
+# opObj11.addParameter(name='zmin', value='25', format='int')
+# opObj11.addParameter(name='zmax', value='40', format='int')
+
+
+
 opObj11 = procUnitConfObj1.addOperation(name='RTIPlot', optype='other')
 opObj11.addParameter(name='id', value='1', format='int')
 opObj11.addParameter(name='wintitle', value='Imaging', format='str')
 opObj11.addParameter(name='showprofile', value='0', format='int')
-opObj11.addParameter(name='xmin', value='19', format='float')
-opObj11.addParameter(name='xmax', value='24', format='float')
-opObj11.addParameter(name='zmin', value='25', format='float')
-opObj11.addParameter(name='zmax', value='40', format='float')
-opObj11.addParameter(name='save', value='1', format='int')
-opObj11.addParameter(name='figpath', value='/home/dsuarez/Pictures/Imaging_Driver4', format='str')
-opObj11.addParameter(name='wr_period', value='5', format='int')
- 
-opObj11 = procUnitConfObj1.addOperation(name='SpectraWriter', optype='other')
-opObj11.addParameter(name='path', value='/media/datos/IMAGING/IMAGING/Driver4/')
-opObj11.addParameter(name='blocksPerFile', value='10', format='int')
+opObj11.addParameter(name='xmin', value=xmin, format='float')
+opObj11.addParameter(name='xmax', value=xmax, format='float')
+opObj11.addParameter(name='zmin', value=zmin, format='float')
+opObj11.addParameter(name='zmax', value=zmax, format='float')
+
+if save_figure:
+    opObj11.addParameter(name='save', value='1', format='int')
+    opObj11.addParameter(name='figpath', value=gpath, format='str')
+    #opObj11.addParameter(name='figpath', value='/home/dsuarez/Pictures/Imaging_Driver4', format='str')
+    opObj11.addParameter(name='wr_period', value='5', format='int')  
+
+if save_pdata:
+    opObj11 = procUnitConfObj1.addOperation(name='SpectraWriter', optype='other')
+    opObj11.addParameter(name='path', value=wpath)
+    #opObj11.addParameter(name='path', value='/media/datos/IMAGING/IMAGING/Driver4/')
+    opObj11.addParameter(name='blocksPerFile', value='10', format='int')
 
 
 # print "Escribiendo el archivo XML"
