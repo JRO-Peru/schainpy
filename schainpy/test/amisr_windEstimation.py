5d24178b6c75fdae46a2b59391b7aafa0cc50170
Julio Valdez <julio.oscanoa@jro.igp.gob.pe>
2014-10-22 14:43:41 +0000

    AMISR wind estimation script updated

--- schainpy/test/amisr_windEstimation.py
+++ schainpy/test/amisr_windEstimation.py
@@ -18,14 +18,14 @@ path = os.path.join(os.environ['HOME'],'Development/amisr/data')
 path = '/home/soporte/Data/AMISR'
 figpath = os.path.join(os.environ['HOME'],'Pictures/amisr')
 
-pathFigure = '/home/soporte/workspace/Graficos/DBS/amisr/'
-xmin = '16.0'
-xmax = '17.0'
+pathFigure = '/home/operaciones/Documents/AMISR_windprofiler/20141023'
+xmin = '8.0'
+xmax = '12.0'
 
 readUnitConfObj = controllerObj.addReadUnit(datatype='AMISRReader',
                                             path=path,
-                                            startDate='2014/10/21',
-                                            endDate='2014/10/21',
+                                            startDate='2014/10/23',
+                                            endDate='2014/10/23',
                                             startTime='00:00:00',
                                             endTime='23:59:59',
                                             walk=1,
@@ -85,12 +85,13 @@ opObj11 = procUnitConfObjSpectraBeam0.addOperation(name='RTIPlot', optype='other
 opObj11.addParameter(name='id', value='200', format='int')
 # opObj11.addParameter(name='wintitle', value=title0, format='str')
 opObj11.addParameter(name='showprofile', value='0', format='int')
-#Setting RTI time using xmin,xmax
 opObj11.addParameter(name='xmin', value=xmin, format='float')
 opObj11.addParameter(name='xmax', value=xmax, format='float')
-# Setting dB range with zmin, zmax
-opObj11.addParameter(name='zmin', value='45', format='int')
-opObj11.addParameter(name='zmax', value='70', format='int')
+opObj11.addParameter(name='zmin', value='30', format='int')
+opObj11.addParameter(name='zmax', value='80', format='int')
+opObj23.addParameter(name='save', value='1', format='bool')
+opObj23.addParameter(name='figpath', value = pathFigure, format='str')
+
 # Save RTI
 # figfile0 = 'amisr_rti_beam0.png'
 # opObj11.addParameter(name='figpath', value=figpath, format='str')
@@ -112,8 +113,8 @@ opObj22 = procUnitConfObj2.addOperation(name='WindProfiler', optype='other')
 opObj22.addParameter(name='technique', value='DBS', format='str')
 opObj22.addParameter(name='correctAzimuth', value='51.06', format='float')
 opObj22.addParameter(name='correctFactor', value='-1', format='float') 
-opObj22.addParameter(name='azimuth', value='0,180,90,0,-90', format='floatlist') 
-opObj22.addParameter(name='elevation', value='74.53,75.60,75.60,90.0,75.60', format='floatlist')
+opObj22.addParameter(name='azimuth', value='0,-90,0,90,180', format='floatlist') 
+opObj22.addParameter(name='elevation', value='74.53,75.90.0,75.60,75.60', format='floatlist')
 # opObj22.addParameter(name='horizontalOnly', value='1', format='bool')
 # opObj22.addParameter(name='channelList', value='1,2', format='intlist')
 
@@ -126,9 +127,9 @@ opObj23.addParameter(name='zmin', value='-20', format='int')
 opObj23.addParameter(name='zmax', value='20', format='int')
 opObj23.addParameter(name='zmin_ver', value='-100', format='float')
 opObj23.addParameter(name='zmax_ver', value='100', format='float')
-opObj23.addParameter(name='SNRmin', value='-10', format='int')
-opObj23.addParameter(name='SNRmax', value='60', format='int')
-opObj23.addParameter(name='SNRthresh', value='0', format='float')
+opObj23.addParameter(name='SNRmin', value='-20', format='int')
+opObj23.addParameter(name='SNRmax', value='30', format='int')
+opObj23.addParameter(name='SNRthresh', value='-50', format='float')
 opObj23.addParameter(name='xmin', value=xmin, format='float')
 opObj23.addParameter(name='xmax', value=xmax, format='float')
 
