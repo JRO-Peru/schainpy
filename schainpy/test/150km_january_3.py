b2c701a5a229d62aaa0b4c276a395a766300c906
Miguel Valdez <miguel.urco@jro.igp.gob.pe>
2015-01-13 12:35:04 +0000

    Updating  programm running on 14 January 2015

--- schainpy/test/150km_january_3.py
+++ schainpy/test/150km_january_3.py
@@ -14,17 +14,17 @@ controllerObj = Project()
 
 controllerObj.setup(id = '191', name='test01', description=desc)
 
-path = '/Volumes/DATA/RAW_EXP/2015_ISR'
+path = '/home/operaciones/150km_jicamarca_january/RAW_EXP/2015_ISR'
 
 figpath = '/home/operaciones/Pictures/150km_jicamarca_january'
 
 readUnitConfObj = controllerObj.addReadUnit(datatype='VoltageReader',
                                             path=path,
-                                            startDate='2015/01/13',
+                                            startDate='2015/01/14',
                                             endDate='2015/01/30',
-                                            startTime='15:00:00',
+                                            startTime='07:40:00',
                                             endTime='23:59:59',
-                                            online=0,
+                                            online=1,
                                             delay=10,
                                             walk=1,
                                             getblock=1)
@@ -33,27 +33,69 @@ opObj11 = readUnitConfObj.addOperation(name='printNumberOfBlock')
 
 procUnitConfObj0 = controllerObj.addProcUnit(datatype='VoltageProc', inputId=readUnitConfObj.getId())
 
-# opObj11 = procUnitConfObj0.addOperation(name='ProfileSelector', optype='other')
-# opObj11.addParameter(name='profileRangeList', value='0,20', format='intlist')
 
+
+a=[]
+for i in range(21):
+    a.append(i)      
+for i in range(106):
+    if i>84:
+        a.append(i)  
+for i in range(211):   
+    if i>189:
+       a.append(i)
+for i in range(316):   
+    if 315>i>294:
+       a.append(i) 
+    if i==315:
+        a.append(i)
+
+        
+b= str(a) 
+profileIndex =   b[1:][:-1]
+print profileIndex
 
 opObj11 = procUnitConfObj0.addOperation(name='ProfileSelector', optype='other')
 # profileIndex =  '20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99'
-# opObj11.addParameter(name='profileList', value=profileIndex, format='intlist')
-opObj11.addParameter(name='profileRangeList', value='0,20', format='intlist')
+opObj11.addParameter(name='profileList', value=profileIndex, format='intlist')
+
+#opObj11.addParameter(name='profileRangeList', value='0,20', format='intlist')
 opObj11.addParameter(name='byblock', value='1', format='bool')
       
 # opObj11 = procUnitConfObjISR.addOperation(name='ProfileConcat', optype='other')
 # opObj11.addParameter(name='m', value='5', format='int')
    
 opObj11 = procUnitConfObj0.addOperation(name='Reshaper', optype='other') #Esta Operacion opera sobre bloques y reemplaza el ProfileConcat que opera sobre perfiles
-opObj11.addParameter(name='shape', value='8,84,140', format='intlist') # shape = (nchannels, nprofiles, nhieghts)
+opObj11.addParameter(name='shape', value='8,336,140', format='intlist') # shape = (nchannels, nprofiles, nhieghts)
 
+
+a=[]
+for i in range(81):
+    if i>0:
+        a.append(i)
+      
+for i in range(165):
+    if i>84:
+        a.append(i)  
+for i in range(249):   
+    if i>168:
+       a.append(i)
+for i in range(333):   
+    if 332>i>252:
+       a.append(i) 
+    if i==332:
+        a.append(i)
+
+        
+b= str(a) 
+profileIndex1 =   b[1:][:-1]
+
+print profileIndex1
 
 opObj11 = procUnitConfObj0.addOperation(name='ProfileSelector', optype='other')
 # profileIndex =  '20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99'
-# opObj11.addParameter(name='profileList', value=profileIndex, format='intlist')
-opObj11.addParameter(name='profileRangeList', value='1,80', format='intlist')
+opObj11.addParameter(name='profileList', value=profileIndex1, format='intlist')
+#opObj11.addParameter(name='profileRangeList', value='1,80', format='intlist')
 opObj11.addParameter(name='byblock', value='1', format='bool')
 
     
@@ -69,7 +111,7 @@ opObj11.addParameter(name='code', value=cod7barker, format='floatlist')
 opObj11.addParameter(name='nCode', value='4', format='int')
 opObj11.addParameter(name='nBaud', value='7', format='int')
 opObj11.addParameter(name='mode', value='3', format='int')
-opObj11.addParameter(name='times', value='20', format='int') 
+opObj11.addParameter(name='times', value='80', format='int') 
 opObj11.addParameter(name='osamp', value='1', format='int')
 
 opObj11 = procUnitConfObj0.addOperation(name='deFlip')
@@ -96,40 +138,58 @@ opObj11.addParameter(name='channelList', value='1,3,5,7', format='intlist')
 
 procUnitConfObj1 = controllerObj.addProcUnit(datatype='SpectraProc', inputId=procUnitConfObj0.getId())
 procUnitConfObj1.addParameter(name='nFFTPoints', value='80', format='int')
-procUnitConfObj1.addParameter(name='nProfiles', value='80', format='int')
-procUnitConfObj1.addParameter(name='pairsList', value='(0,1),(2,3),(4,5),(6,7)', format='pairsList')
+procUnitConfObj1.addParameter(name='nProfiles', value='320', format='int')
 
-opObj11 = procUnitConfObj1.addOperation(name='IncohInt', optype='other')
-opObj11.addParameter(name='timeInterval', value='10', format='float')
+procUnitConfObj1.addParameter(name='pairsList', value='(1,0),(3,2),(5,4),(7,6)', format='pairsList')
+
 # 
-opObj11 = procUnitConfObj1.addOperation(name='SpectraPlot', optype='other')
-opObj11.addParameter(name='id', value='2004', format='int')
-opObj11.addParameter(name='wintitle', value='150km_Jicamarca_ShortPulse', format='str')
-#opObj11.addParameter(name='channelList', value='0,1,2,3,45', format='intlist')
-#opObj11.addParameter(name='zmin', value='15', format='int')
-#  opObj11.addParameter(name='zmax', value='45', format='int')
+opObj11 = procUnitConfObj1.addOperation(name='IncohInt', optype='other')
+opObj11.addParameter(name='timeInterval', value='60', format='float')
+# 
+# opObj11 = procUnitConfObj1.addOperation(name='SpectraPlot', optype='other')
+# opObj11.addParameter(name='id', value='2004', format='int')
+# opObj11.addParameter(name='wintitle', value='150km_Jicamarca_ShortPulse', format='str')
+# #opObj11.addParameter(name='channelList', value='0,1,2,3,45', format='intlist')
+# opObj11.addParameter(name='zmin', value='15', format='int')
+# opObj11.addParameter(name='zmax', value='45', format='int')
 # opObj11.addParameter(name='figpath', value=figpath, format='str')
 # opObj11.addParameter(name='exp_code', value='13', format='int')
 
 
 opObj11 = procUnitConfObj1.addOperation(name='CrossSpectraPlot', optype='other')
-opObj11.addParameter(name='id', value='2005', format='int')
+opObj11.addParameter(name='id', value='2006', format='int')
 opObj11.addParameter(name='wintitle', value='CrossSpectraPlot_ShortPulse', format='str')
-# opObj11.addParameter(name='figpath', value=figpath, format='str')
-# opObj11.addParameter(name='exp_code', value='13', format='int')
+opObj11.addParameter(name='ymin', value='0', format='int')
+opObj11.addParameter(name='ymax', value='105', format='int')
 
+opObj11.addParameter(name='zmin', value='15', format='int')
+opObj11.addParameter(name='zmax', value='45', format='int')
+opObj11.addParameter(name='figpath', value=figpath, format='str')
+opObj11.addParameter(name='exp_code', value='13', format='int')
+
+
+opObj11 = procUnitConfObj1.addOperation(name='CoherenceMap', optype='other')
+opObj11.addParameter(name='id', value='102', format='int')
+opObj11.addParameter(name='wintitle', value='Coherence', format='str')
+opObj11.addParameter(name='phase_cmap', value='jet', format='str')
+
+# 
+# opObj11.addParameter(name='xmin', value='0', format='int')
+# opObj11.addParameter(name='xmax', value='24', format='int')
+# opObj11.addParameter(name='figpath', value=figpath, format='str')
+# opObj11.addParameter(name='wr_period', value='2', format='int')
 
   
-opObj11 = procUnitConfObj1.addOperation(name='RTIPlot', optype='other')
-opObj11.addParameter(name='id', value='3005', format='int')
-opObj11.addParameter(name='wintitle', value='150km_Jicamarca_ShortPulse', format='str')
-# opObj11.addParameter(name='xmin', value='20.5', format='float')
-# opObj11.addParameter(name='xmax', value='24', format='float')
+# opObj11 = procUnitConfObj1.addOperation(name='RTIPlot', optype='other')
+# opObj11.addParameter(name='id', value='3005', format='int')
+# opObj11.addParameter(name='wintitle', value='150km_Jicamarca_ShortPulse', format='str')
+# # opObj11.addParameter(name='xmin', value='20.5', format='float')
+# # opObj11.addParameter(name='xmax', value='24', format='float')
 # opObj11.addParameter(name='zmin', value='15', format='int')
 # opObj11.addParameter(name='zmax', value='45', format='int')
-#opObj11.addParameter(name='channelList', value='0,1,2,3', format='intlist')
-#opObj11.addParameter(name='channelList', value='0,1,2,3,4,5,6,7', format='intlist')
-opObj11.addParameter(name='showprofile', value='0', format='int')
+# #opObj11.addParameter(name='channelList', value='0,1,2,3', format='intlist')
+# #opObj11.addParameter(name='channelList', value='0,1,2,3,4,5,6,7', format='intlist')
+# opObj11.addParameter(name='showprofile', value='0', format='int')
 # opObj11.addParameter(name='figpath', value=figpath, format='str')
 # opObj11.addParameter(name='exp_code', value='13', format='int')
 
