2f3b5a39c6d4859a2fce31cef139d9b154696e1b
Miguel Valdez <miguel.urco@jro.igp.gob.pe>
2016-01-13 01:47:04 +0000

    mpldriver: minor changes

--- schainpy/model/graphics/mpldriver.py
+++ schainpy/model/graphics/mpldriver.py
@@ -331,8 +331,11 @@ def createPmultilineYAxis(ax, x, y, xmin, xmax, ymin, ymax, xlabel='', ylabel=''
     
 #    lines = ax.plot(x, y.T, marker=marker,markersize=markersize,linestyle=linestyle)
     lines = ax.plot(x, y.T)
-    leg = ax.legend(lines, legendlabels, loc=1, bbox_to_anchor=(1.01, 1.00), numpoints=1, handlelength=1.5, \
-                    handletextpad=0.5, borderpad=0.5, labelspacing=0.5, borderaxespad=0.)
+#     leg = ax.legend(lines, legendlabels, loc=2, bbox_to_anchor=(1.01, 1.00), numpoints=1, handlelength=1.5, \
+#                     handletextpad=0.5, borderpad=0.5, labelspacing=0.5, borderaxespad=0.)
+    
+    leg = ax.legend(lines, legendlabels,
+                    loc='upper left', bbox_to_anchor=(0.75, 1), borderaxespad=0)
     
     for label in leg.get_texts(): label.set_fontsize(9)
     
