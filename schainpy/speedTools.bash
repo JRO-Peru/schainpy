a852583c4e96c60995e8913d6c1fde89603eab61
Miguel Valdez <miguel.urco@jro.igp.gob.pe>
2016-02-15 15:16:47 +0000

    speedTools.bash updated and tested

--- schainpy/speedTools.bash
+++ schainpy/speedTools.bash
@@ -1,8 +1,11 @@
+#Use this script in order to find the slowest module or function in your python code.
+#Reference: http://lukauskas.co.uk/articles/2014/02/12/how-to-make-python-faster-without-trying-that-much/
+
 #!/bin/sh
 
-filename = "testRawData.py"
-output = "output"
+script = "testRawData.py"
+output = "profile.pdf"
 
-python -m cProfile -o $output $filename
+python -m cProfile -o profile.pstats $script
 
-gprof2dot -f pstats $output.pstats | dot -Tpng -o $output.png
+gprof2dot -f pstats profile.pstats | dot -Tpdf -o $output
