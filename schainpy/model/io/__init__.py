85687bb4db12b910e16350a4726b8f7b2c07b166
Julio Valdez <julio.oscanoa@jro.igp.gob.pe>
2016-09-28 21:24:58 +0000

    Changed name from jroIO_HDF5 to jroIO_param

--- schainpy/model/io/__init__.py
+++ schainpy/model/io/__init__.py
@@ -9,6 +9,6 @@ from jroIO_spectra import *
 from jroIO_heispectra import *
 from jroIO_usrp import *
     
-from jroIO_amisr import *
-from jroIO_HDF5 import *
+from jroIO_kamisr import *
+from jroIO_param import *
 from jroIO_hf import *
