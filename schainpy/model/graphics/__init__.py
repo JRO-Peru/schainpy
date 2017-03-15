e332a4152b27303b6ae5de38f5275550512f6c33
Juan C. Valdez <juan.espinoza@jro.igp.gob.pe>
2017-03-15 03:10:23 +0000

    add jroplot_data

--- schainpy/model/graphics/__init__.py
+++ schainpy/model/graphics/__init__.py
@@ -3,5 +3,6 @@ from jroplot_spectra import *
 from jroplot_heispectra import *
 from jroplot_correlation import *
 from jroplot_parameters import *
+from jroplot_data import *
 from jroplotter import *
     
